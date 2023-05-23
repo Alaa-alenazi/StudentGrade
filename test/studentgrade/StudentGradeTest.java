297e77b3a336143a6249037b3dc4bc3af7bfc4be
Alaa Alenazi <>
2023-05-23 15:20:43 +0300

    Apply JUINT-4 for StudentGradeTest

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,35 +1,70 @@
+
 package studentgrade;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+/**
+ *
+ * @author Alaa AlEnazi
+ */
+public class StudentGradeTest{
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
-    public void testGetGrade95() {
-        System.out.println("getGrade 95");
-        int mark = 95;
-        String expResult = "A";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
-    }
-
-    public void testGetGrade85() {
-        System.out.println("getGrade 85");
-        int mark = 85;
-        String expResult = "B";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+    int mark;
+    String expectedResult, actualResult;
+    
+    public StudentGradeTest(){
     }
     
-    public void testGetGrade75() {
-        System.out.println("getGrade 85");
-        int mark = 75;
-        String expResult = "F";
-        String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+    @BeforeClass
+    public static void beforeTestClass() {
+        System.out.println("Before Test Class, (This will be executed once in the class).");
+        System.out.println();
+    }
+    
+    
+    @Before
+    public void beforeTestCase() {
+        System.out.println("Before Test Case, (This will be executed before each Test Case).");
+    }
+    
+
+    /**
+     * Test of getGrade method, Mark = 99, of class StudentGrade.
+     */
+    @Test
+    public void testGetGrade99() {
+       System.out.println("Test Get Grade 99");
+       mark = 99;
+       expectedResult = "A";
+       actualResult = StudentGrade.getGrade(mark);
+       assertEquals(expectedResult, actualResult);
+    }
+    
+    /**
+     * Test of getGrade method, Mark = 80, of class StudentGrade.
+     */
+    @Test
+    public void testGetGrade80() {
+       System.out.println("Test Get Grade 80");
+       mark = 80;
+       expectedResult = "B";
+       actualResult = StudentGrade.getGrade(mark);
+       assertEquals(expectedResult, actualResult);
+    }
+    
+    /**
+     * Test of getGrade method, Mark = 62, of class StudentGrade.
+     */
+    @Test
+    public void testGetGrade62() {
+       System.out.println("Test Get Grade 62");
+       mark = 62;
+       expectedResult = "F";
+       actualResult = StudentGrade.getGrade(mark);
+       assertEquals(expectedResult, actualResult);
     }
 }
